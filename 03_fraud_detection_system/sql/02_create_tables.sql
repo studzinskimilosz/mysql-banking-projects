b1cecb3097c95537d2cfd73613b0321d1e0cba8a
Milosz Studzinski <studzinskimilosz@gmail.com>
2026-08-25 19:02:59 +0200

    feat: add Accounts table

--- 03_fraud_detection_system/sql/02_create_tables.sql
+++ 03_fraud_detection_system/sql/02_create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -7,4 +7,17 @@ CREATE TABLE customers (
     phone_number VARCHAR(20) NOT NULL UNIQUE,
     country ENUM( 'PL', 'DE', 'GB', 'US', 'FR', 'ES'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE Accounts(
+    account_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    account_number VARCHAR(26) NOT NULL UNIQUE,
+    account_type VARCHAR(50) NOT NULL,
+    currency CHAR(3) NOT NULL DEFAULT 'PLN',
+    balance DECIMAL(15,2) NOT NULL DEFAULT 0.00,
+    account_status VARCHAR(50) DEFAULT 'active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id)
+    REFERENCES Customers(customer_id)
 )
