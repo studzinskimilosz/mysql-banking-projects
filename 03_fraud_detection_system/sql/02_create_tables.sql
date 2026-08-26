79e4e2da8c8ecf1f173be92d78245166306822a1
Milosz Studzinski <studzinskimilosz@gmail.com>
2026-08-26 11:51:35 +0200

    feat: create Cards table

--- 03_fraud_detection_system/sql/02_create_tables.sql
+++ 03_fraud_detection_system/sql/02_create_tables.sql
@@ -5,11 +5,11 @@ CREATE TABLE Customers (
     date_of_birth DATE NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     phone_number VARCHAR(20) NOT NULL UNIQUE,
-    country ENUM( 'PL', 'DE', 'GB', 'US', 'FR', 'ES'),
+    country ENUM('PL', 'DE', 'GB', 'US', 'FR', 'ES'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
-CREATE TABLE Accounts(
+CREATE TABLE Accounts (
     account_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT NOT NULL,
     account_number VARCHAR(26) NOT NULL UNIQUE,
@@ -20,4 +20,16 @@ CREATE TABLE Accounts(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (customer_id)
     REFERENCES Customers(customer_id)
+)
+
+CREATE TABLE Cards (
+    card_id INT PRIMARY KEY AUTO_INCREMENT,
+    account_id INT NOT NULL,
+    card_number VARCHAR(20), UNIQUE NOT NULL,
+    card_type ENUM('DEBIT', 'CREDIT'),
+    status ENUM('ACTIVE', 'BLOCKED', 'EXPIRED', 'CANCELLED'),
+    issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expires_at DATE NOT NULL,
+    FOREIGN KEY (account_id)
+    REFERENCES Accounts(account_id)
 )
