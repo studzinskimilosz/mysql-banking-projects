8eeb06092138905a493aef6eaaf423d64b928cf7
Milosz Studzinski <studzinskimilosz@gmail.com>
2026-08-26 11:56:53 +0200

    feat: add Merchants table

--- 03_fraud_detection_system/sql/02_create_tables.sql
+++ 03_fraud_detection_system/sql/02_create_tables.sql
@@ -5,9 +5,9 @@ CREATE TABLE Customers (
     date_of_birth DATE NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     phone_number VARCHAR(20) NOT NULL UNIQUE,
-    country ENUM('PL', 'DE', 'GB', 'US', 'FR', 'ES'),
+    country ENUM('PL', 'DE', 'GB', 'US', 'FR', 'ES') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE Accounts (
     account_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -20,16 +20,36 @@ CREATE TABLE Accounts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (customer_id)
     REFERENCES Customers(customer_id)
-)
+);
 
 CREATE TABLE Cards (
     card_id INT PRIMARY KEY AUTO_INCREMENT,
     account_id INT NOT NULL,
     card_number VARCHAR(20), UNIQUE NOT NULL,
-    card_type ENUM('DEBIT', 'CREDIT'),
-    status ENUM('ACTIVE', 'BLOCKED', 'EXPIRED', 'CANCELLED'),
+    card_type ENUM('DEBIT', 'CREDIT') NOT NULL,
+    status ENUM('ACTIVE', 'BLOCKED', 'EXPIRED', 'CANCELLED') NOT NULL,
     issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expires_at DATE NOT NULL,
     FOREIGN KEY (account_id)
     REFERENCES Accounts(account_id)
-)
+);
+
+CREATE TABLE Merchants (
+    merchant_id INT PRIMARY KEY AUTO_INCREMENT,
+    merchant_name VARCHAR(100) NOT NULL,
+    merchant_category ENUM(
+        'GROCERY',
+        'RESTAURANT',
+        'FUEL',
+        'TRAVEL',
+        'ELECTRONICS',
+        'ENTERTAINMENT',
+        'ONLINE_SERVICES',
+        'CRYPTO',
+        'GAMBLING',
+        'ATM'
+    ) NOT NULL,
+    country CHAR(2) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
