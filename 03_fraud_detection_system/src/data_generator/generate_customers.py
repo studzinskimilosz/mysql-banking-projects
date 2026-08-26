dddb63b918ce0725f152b8a8275159b3ad2ca37e
Milosz Studzinski <studzinskimilosz@gmail.com>
2026-08-26 22:15:21 +0200

    feat: create function to generate random customers

--- 03_fraud_detection_system/src/data_generator/generate_customers.py
+++ 03_fraud_detection_system/src/data_generator/generate_customers.py
@@ -1,3 +1,33 @@
+import locale
+
 from faker import Faker
+import random
 
 fake = Faker("pl_PL")
+
+locales = {
+    "PL": "pl_PL",
+    "DE": "de_DE",
+    "GB": "en_GB",
+    "US": "en_US",
+    "FR": "fr_FR",
+    "ES": "es_ES"
+}
+
+customers = []
+
+for i in range(100):
+    country = random.choice(list(locales.keys()))
+    fake = Faker(locales[country])
+
+    customer = {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "date_of_birth": fake.date_of_birth(),
+        "email": fake.email(),
+        "phone_number": fake.phone_number(),
+        "country": country,
+    }
+
+    customers.append(customer)
+
